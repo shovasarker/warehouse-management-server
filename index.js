3a0209f67d6126d8bb4971d8d86a752215a2fd06
ShovaSarker <shovasarker01@gmail.com>
2022-05-07 15:04:18 +0600

    Mongodb database connect completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -12,6 +13,25 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Warehouse Management Server is Running')
 })
+
+//mongodb connect
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.87qpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+const run = async () => {
+  try {
+    await client.connect()
+  } finally {
+  }
+}
+
+run().catch(console.dir)
 
 // listneing to a fixed port for the server
 app.listen(port, () => {
