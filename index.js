5d936f86bafc7b8160e51ee56b6542f377fd5845
ShovaSarker <shovasarker01@gmail.com>
2022-05-08 14:35:22 +0600

    Simple JWT verification added for getting items with supplierEmail

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ app.get('/', (req, res) => {
   res.send('Warehouse Management Server is Running')
 })
 
+//verification of jwt
+
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(401).send({
+      message: 'Unauthorized Access',
+    })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.send(403).send({ message: 'Forbidden Access' })
+    }
+    req.decoded = decoded
+
+    next()
+  })
+}
+
 //mongodb connect
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.87qpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -93,6 +115,20 @@ const run = async () => {
     app.post('/item', async (req, res) => {
       const newItem = req.body
       const result = await itemsCollection.insertOne(newItem)
+
+      res.send(result)
+    })
+
+    app.get('/item', verifyJwt, async (req, res) => {
+      const decodedEmail = req.decoded.email
+      const query = req.query
+      if (decodedEmail !== query.supplierEmail) {
+        return res.status(403).send({ message: 'Forbidden Access' })
+      }
+
+      console.log(query)
+      const cursor = itemsCollection.find(query)
+      const result = await cursor.toArray()
 
       res.send(result)
     })
